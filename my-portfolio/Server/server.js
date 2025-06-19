d8b8a906d3d64cf1c5e7bb957d461e2d944738c9
Keith Clarence Sison <sisonkeithclarence@gmail.com>
2025-06-19 13:31:15 +0800

    initial commit
    Fix: Enable CORS for frontend

--- my-portfolio/Server/server.js
+++ my-portfolio/Server/server.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const emailRoute = require("./Routes/email")
 
 const app = express();
-app.use(cors());
+const PORT = process.env.PORT || 3001;
+
+app.use(cors({
+    origin: "https://keithclarencesison-portfolio.vercel.app"
+}));
 app.use(express.json());
 
 app.use("/api", emailRoute);
@@ -17,9 +21,8 @@ app.get("/", (req, res) => {
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
-const PORT = process.env.PORT || 3001;
-console.log("📂 Current working directory:", process.cwd());
-console.log("📧 MAIL_USER =", process.env.MAIL_USER); 
+// console.log("📂 Current working directory:", process.cwd());
+// console.log("📧 MAIL_USER =", process.env.MAIL_USER); 
 
 
 app.listen(PORT, () => {
